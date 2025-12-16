dd3aab20d7e071e4721080af20e6d3d25c2893d1
Paramee Hansana <hansanatp@gmail.com>
2025-12-16 21:15:33 +0530

    Update authMiddleware.js
    auth changes

--- BACKEND/middleware/authMiddleware.js
+++ BACKEND/middleware/authMiddleware.js
@@ -1,6 +1,6 @@
 // middleware/authMiddleware.js
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import User from '../models/user.js';
 
 // Protect routes
 export const protect = async (req, res, next) => {
